7ad78739056aee8771396b1c04117918b45d6eb9
IQ-OFFICE\ibabekov <IQ-OFFICE\ibabekov>
2009-11-21 11:15:49 -0500

    {7/9/2007 10:45:12 AM}

--- Sites/boa.hodesiq.com/App_Code/boanet_safebutton.cs
+++ Sites/boa.hodesiq.com/App_Code/boanet_safebutton.cs
@@ -19,7 +19,7 @@ public static class boanet_safebutton
 	/// </summary>
 	/// <param name="id"></param>
 	/// <param name="placeholderID"></param>
-	public static void writeBOASafeButton(string id, PlaceHolder ph, string buttontext, EventHandler onclickserverfunction, HttpRequest request)
+    public static void writeBOASafeButton(string id, PlaceHolder ph, string buttontext, EventHandler onclickserverfunction, HttpRequest request, string LinkURL)
 	{
 		//evaluate the browser type based on user agent and apply correct stylesheet.
 		//Based on javascript in hs_safebuttons.js file.
@@ -44,17 +44,18 @@ public static class boanet_safebutton
 		btnSubmit.Text = buttontext;
         btnSubmit.ToolTip = buttontext;
 		btnSubmit.CssClass = "btn";
-		btnSubmit.Click += new EventHandler(onclickserverfunction);
+        btnSubmit.Click += new EventHandler(onclickserverfunction);
 
 		//add the button to the noscript tag
 		noscripttag.Controls.Add(btnSubmit);
 
-		//now lets build the "boa" button...
+        //now lets build the "boa" button... 
 		HtmlTable t = new HtmlTable();
 		t.CellPadding = 0;
 		t.CellSpacing = 0;
 		t.Border = 0;
 		t.Attributes.Add("summary", string.Empty);
+
 		HtmlTableRow tr = new HtmlTableRow();
 		HtmlTableCell td = new HtmlTableCell();
 		td.Attributes.Add("class", "whitebg");
@@ -63,13 +64,23 @@ public static class boanet_safebutton
 		d.Attributes.Add("style", "display: none");
 		d.ID = "d" + id;
 		LinkButton l = new LinkButton();
-		l.Click += new EventHandler(onclickserverfunction);
-		l.Attributes.Add("class", cssType + "-btn");
-		l.Attributes.Add("onfocus", "bt_rollover(this,'" + cssType + "-btn-over" + "')");
-		l.Attributes.Add("onBlur", "bt_rollover(this,'" + cssType + "-btn" + "')");
-		l.Text = buttontext;
+        ///apply buttons on job detail page must be 
+        
+        l.Attributes.Add("class", cssType + "-btn");
+        l.Attributes.Add("onfocus", "bt_rollover(this,'" + cssType + "-btn-over" + "')");
+        l.Attributes.Add("onBlur", "bt_rollover(this,'" + cssType + "-btn" + "')");
+        l.Text = buttontext;
         l.ToolTip = buttontext;
-		l.ID = "lb" + id;
+        l.ID = "lb" + id;
+
+        if (buttontext == " Apply Now ")
+        {
+            l.Attributes.Add("onclick", "javascript:window.open('" + LinkURL + "','ApplyNow')");
+        }
+        else
+        {
+            l.Click += new EventHandler(onclickserverfunction);
+        }
 		d.Controls.Add(l);
 		td.Controls.Add(d);
 		tr.Cells.Add(td);
