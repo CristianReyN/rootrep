5ef491bc2f8a0a552f932666f0d30571a5b05b93
IQ-OFFICE\ibabekov <IQ-OFFICE\ibabekov>
2009-11-21 11:05:03 -0500

    {3/26/2007 4:46:12 PM}

--- Sites/boa.hodesiq.com/JobDetails.aspx.cs
+++ Sites/boa.hodesiq.com/JobDetails.aspx.cs
@@ -47,7 +47,7 @@ public partial class JobDetails : System.Web.UI.Page
             lblLocationFooter.Text = dt.Rows[0]["Location"].ToString();
             lblPartTimeFullTime.Text = dt.Rows[0]["FullPartTime"].ToString();
             lblPostingDate.Text = dt.Rows[0]["PostingDate"].ToString();
-            lblSchedule.Text = dt.Rows[0]["Shift"].ToString();
+            lblSchedule.Text = dt.Rows[0]["WeeklySchedule"].ToString();
             lblShift.Text = dt.Rows[0]["Shift"].ToString();
             lblTravel.Text = dt.Rows[0]["Travel"].ToString();
             lblUnpostingDate.Text = dt.Rows[0]["UnPostingDate"].ToString() ;
