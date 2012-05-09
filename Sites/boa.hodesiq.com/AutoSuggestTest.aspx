8110909888fcea8ab25337b562af95595be2b9b5
IQ-OFFICE\dnguyen <IQ-OFFICE\dnguyen>
2012-05-09 17:45:21 -0400

    Autosuggest job search for new homepage redesign

---
+++ Sites/boa.hodesiq.com/AutoSuggestTest.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AutoSuggestTest.aspx.cs" Inherits="AutoSuggestTest" %>
+
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="asp" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="jQueryThemes/start/jquery-ui-1.8.9.custom.css" type="text/css" />
+    <style type="text/css">
+input { padding:4px; }
+table { text-align:left; margin:0px 10px; width:900px; }
+table td, table th { border:1px solid #ddd; padding:5px; }
+
+/****
+*	Custom styling for the select menu items with images and extra 
+*	text in them.
+*/
+.txtJobSearch li a img {
+	float:left;
+	margin-right:5px;
+}
+.txtJobSearch li a small {
+	display:block;
+	text-align:left;
+	font-style:italic;
+}
+.txtJobSearch { font-size:0.8em; }
+</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    <asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+    </asp:ToolkitScriptManager>
+    <asp:TextBox ID="txtJobSearch" runat="server" Text="Enter Job Title" ToolTip="Enter Job Title" Enabled="true" OnClick="this.value=''" Height="16px" Width="234px"></asp:TextBox>
+    <asp:AutoCompleteExtender ID="txtJobSearch_AutoCompleteExtender" runat="server" 
+        DelimiterCharacters="" Enabled="True" ServiceMethod="GetCompletionList" 
+        ServicePath="" TargetControlID="txtJobSearch" UseContextKey="True" CompletionInterval="0">
+    </asp:AutoCompleteExtender>
+    <button type=button onclick="window.parent.location='http://localhost/boa/jobsearch.aspx';">Find</button>
+    </form>
+</body>
+</html>
---
+++ Sites/boa.hodesiq.com/AutoSuggestTest.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Net;
+
+public partial class AutoSuggestTest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    [System.Web.Services.WebMethodAttribute(), System.Web.Script.Services.ScriptMethodAttribute()]
+    public static string[] GetCompletionList(string prefixText, int count, string contextKey)
+    {
+        //string[] str = { prefixText + "A", prefixText + "B", prefixText + "C", prefixText + "D" };
+
+        string constring = ConfigurationManager.AppSettings["StrUdlFileName"];
+        OleDbConnection con = new OleDbConnection(constring);
+        con.Open();
+
+        try
+        {
+            OleDbCommand cmd = new OleDbCommand("p_boaJobSearchAutoSuggest2", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@KeyWords", prefixText);
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            string[] jobSuggestions = new string[ds.Tables[0].Rows.Count];
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                jobSuggestions[i] = ds.Tables[0].Rows[i]["JobName"].ToString();
+            }
+
+            //return (from m in jobSuggestions where m.StartsWith(prefixText, StringComparison.CurrentCultureIgnoreCase) select m).Take(count).ToArray();
+            return jobSuggestions;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            con.Close();
+        }
+        
+    }
+}
